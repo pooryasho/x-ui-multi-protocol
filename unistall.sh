9c62a07c9fad0828e2c21203c0d0fe2371fb6b16
pooryasho <142620046+pooryasho@users.noreply.github.com>
2025-11-04 02:28:41 +0330

    Fix uninstallation script for x-ui-multi-protocol

--- unistall.sh
+++ unistall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-    systemctl stop tx-ui-multi-protocol
-    systemctl disable tx-ui-multi-protocol
-    rm -f /etc/systemd/system/tx-ui-multi-protocol.service 
+    systemctl stop x-ui-multi-protocol
+    systemctl disable x-ui-multi-protocol
+    rm -f /etc/systemd/system/x-ui-multi-protocol.service 
     systemctl daemon-reload
     systemctl reset-failed
-    rm -rf /etc/tx-ui-multi-protocol 
-    rm -rf tx-ui-multi-protocol/tx-ui-multi-protocol 
-    sudo apt-get purge dotnet-sdk-7.0
+    rm -rf /etc/x-ui-multi-protocol 
+    rm -rf x-ui-multi-protocol/x-ui-multi-protocol 
+    sudo apt-get purge dotnet-sdk-8.0
      sudo rm -f /etc/apt/sources.list.d/microsoft-prod.list && sudo apt update
 
     echo "Uninstalled Successfully!"
